78fe32af89f60329c9a16b70f7ac84a730de10be
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2025-09-06 16:16:03 -0400

    Task 4: Person data class created

--- main.kt
+++ main.kt
@@ -1,36 +1,56 @@
-enum class Gender {
-    FEMALE,
-    MALE
-}
-
-fun saludar(nombreCompleto: String, saludo: String = "Hola"): Unit {
-   val saludoCompleto: String = "$saludo, mi nombre es $nombreCompleto"
-   println(saludoCompleto)
-}
-
-fun obtenerGenero(genero: Gender): String {
-    
-    var mensaje: String = "Mi genero es "
-    
-    when(genero) {
-        Gender.FEMALE -> mensaje += "Femenino"
-        Gender.MALE -> mensaje += "Masculino"
-    }
-    
-    return mensaje
+enum class Gender(val descripcion: String) {
+    FEMALE("Femenino"),
+    MALE("Masculino")
 }
 
 fun main() {
     val saludo: String = "Bienvenido a la materia de aplicaciones moviles"
-    val nombreCompleto: String = "Rafael Andres Vargas Mamani"
+    
+    val nombre: String = "Rafael Andres"
+    val apellido: String = "Vargas Mamani"
+    val materia: String = "Aplicaciones Moviles"
+    val peso: Double = 60.0
+    val altura: Double = 1.72
+    val edad: Int = 20
     val genero: Gender = Gender.MALE
-    
-    //print -> "Bienvenido a la materia de aplicaciones moviles, mi nombre es Rafael Andres Vargas Mamani"
-    saludar(nombreCompleto = nombreCompleto, saludo = saludo)
 
-    //return -> "Mi genero es Masculino"
-    println(obtenerGenero(genero = genero))
+    val rafael: Persona = Persona(nombre, apellido, materia, peso, altura, edad, genero)
     
-    //print -> "Hola, mi nombre es Rafael Andres Vargas Mamani"
-	saludar(nombreCompleto = nombreCompleto)
+    // print -> Hola, mi nombre es Rafael Andres Vargas Mamani
+    rafael.saludar()
+    
+    // print -> Bievenido a la materia de aplicaciones moviles, mi nombre es Rafael Andres Vargas Mamani
+    rafael.saludar(saludo = "Bienvenido a la materia de aplicaciones moviles")
+    
+    // rafael.esMayorEdad() returns -> true
+   	println("Soy ${if (rafael.esMayorEdad()) "mayor" else "menor"} de edad")
+    
+    // rafael.obtenerGenero() returns -> "Masculino"
+    println("Mi genero es ${rafael.obtenerGenero()}")
+}
+
+
+data class Persona(
+	val nombre: String,
+    val apellido: String,
+    val materia: String,
+    val peso: Double,
+    val altura: Double,
+    val edad: Int,
+    val genero: Gender
+) {
+    fun saludar(saludo: String = "Hola"): Unit {
+        println("$saludo, mi nombre es $nombreCompleto")
+    }
+    
+    fun esMayorEdad(): Boolean {
+        return edad >= 18
+    }
+    
+    fun obtenerGenero(): String {
+        return genero.descripcion
+    }
+    
+    val nombreCompleto: String
+    	get() = "$nombre $apellido"
 }
